183705003d495608b70def171e811557984e04a4
Alan Phelps <phelpsa64@gmail.com>
2022-02-19 19:13:34 -0500

    primary prompts created

---
+++ index.js
@@ -0,0 +1,86 @@
+const { prompt } = require('inquirer');
+const logo = require ('asciiart-logo');
+// const db = require('./db');
+// require('console.table');
+
+init();
+
+function init() {
+    const logoText = logo({ name: 'Employee Tracker' }).render();
+
+    console.log(logoText);
+
+    primaryPrompts();
+}
+
+function primaryPrompts() {
+    prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: [
+                {
+                    name: 'View All Employees',
+                    value: 'VIEW_EMPLOYEES'
+                },
+                {
+                    name: 'View All Employees By Department',
+                    value: 'VIEW_EMPLOYEES_BY_DEPARTMENT'
+                },
+                {
+                    name: 'View All Employees By Manager',
+                    value: 'VIEW_EMPLOYEES_BY_MANAGER'
+                },
+                {
+                    name: 'Add Employee',
+                    value: 'ADD_EMPLOYEE'
+                },
+                {
+                    name: 'Remove Employee',
+                    value: 'REMOVE_EMPLOYEE'
+                },
+                {
+                    name: 'Update Employee Role',
+                    value: 'UPDATE_EMPLOYEE_ROLE'
+                },
+                {
+                    name: 'Update Employee Manager',
+                    value: 'UPDATE_EMPLOYEE_MANAGER'
+                },
+                {
+                    name: 'View All Roles',
+                    value: 'VIEW_ROLES'
+                },
+                {
+                    name: 'Add Role',
+                    value: 'ADD_ROLE'
+                },
+                {
+                    name: 'Remove Role',
+                    value: 'REMOVE_ROLE'
+                },
+                {
+                    name: 'View All Departments',
+                    value: 'VIEW_DEPARTMENTS'
+                },
+                {
+                    name: 'Add Department',
+                    value: 'ADD_DEPARTMENT'
+                },
+                {
+                    name: 'Remove Department',
+                    value: 'REMOVE_DEPARTMENT'
+                },
+                {
+                    name: 'View Total Utilized Budget By Department',
+                    value: 'VIEW_UTILIZED_BUDGET_BY_DEPARTMENT'
+                },
+                {
+                    name: 'Quit',
+                    value: 'QUIT'
+                }                
+            ]
+        }
+    ])
+}
