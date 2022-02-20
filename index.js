3007642b80698c891933030f2888d236d34e275e
Alan Phelps <phelpsa64@gmail.com>
2022-02-19 22:26:48 -0500

    removed unneeded package

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { prompt } = require('inquirer');
 const logo = require ('asciiart-logo');
 const db = require('./db');
-const inquirer = require('inquirer');
 require('console.table');
 
 init();
